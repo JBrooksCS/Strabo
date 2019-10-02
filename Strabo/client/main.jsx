e9ac71d3c752e47c7503fda1c78057e47b2406ce
JakePreston <prestonjak@outlook.com>
2019-10-02 10:59:54 -0500

    file layout

--- Strabo/client/main.jsx
+++ Strabo/client/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import Strabo from '../imports/ui/Srabo';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  render(<Strabo />, document.getElementById('react-target'));
 });
---
+++ Strabo/imports/ui/Srabo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Strabo = () => (
+  <div className="Wrapper-Strabo">
+    <h1>Strabo.</h1>
+  </div>
+);
+
+
+export default Strabo;
