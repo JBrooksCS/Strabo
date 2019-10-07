b3089d49c340bee97adc6627d147294c1e59fd4e
Brandon Bradley <bbradley615@gmail.com>
2019-10-07 13:50:04 -0500

    fix root path redirect logic

--- imports/ui/component/Landing.jsx
+++ imports/ui/component/Landing.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 
-export class Landing extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Landing Page (not logged in)</h1>
-            </div>
-        );
+function Landing({ currentUser }) {
+    const [user, setUser] = useState(currentUser)
+
+    useEffect(() => setUser(currentUser), [currentUser])
+
+    if (user) {
+      return <Redirect to="/dashboard" />
     }
+    return (
+        <div>
+            <h1>Landing Page (not logged in)</h1>
+        </div>
+    );
 }
 
 export default Landing;
--- imports/ui/component/Strabo.jsx
+++ imports/ui/component/Strabo.jsx
@@ -7,24 +7,17 @@ import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 
 
-
-
 class Strabo extends Component {
     render() {
-        // console.log(this.props)
-        // console.log("Strabo Rendering")
-
         return (
             <div className="Wrapper-Strabo">
                 <NavBar />
-                <AppViews />
+                <AppViews currentUser={this.props.currentUser} />
             </div>
         )
     }
 }
 
-
-// export default Strabo;
 export default withTracker(() => {
     return {
         trips: Trips.find({}).fetch(),
