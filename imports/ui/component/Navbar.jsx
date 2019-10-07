90052f298b9f41c1bdfa124cc63a47bf545b2d1a
Brandon Bradley <bbradley615@gmail.com>
2019-10-07 12:52:05 -0500

    refactor Auth component away

--- imports/ui/component/Navbar.jsx
+++ imports/ui/component/Navbar.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import {Auth} from "./Auth"
+import AccountsUIWrapper from '../AccountsUIWrapper.jsx';
 
 export default class NavBar extends Component {
     render() {
         return (
             <div className="Wrapper-NavBar">
-
-
                 <h1>Strabo</h1>
-                <Auth />
-
+                <AccountsUIWrapper />
             </div>
         )
     }
