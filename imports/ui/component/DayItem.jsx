e64e0c9727a5c57888aacfd8a88b3f58d82440bf
JakePreston <prestonjak@outlook.com>
2019-10-09 11:29:23 -0500

    day array

--- imports/ui/component/DayItem.jsx
+++ imports/ui/component/DayItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 export default function DayItem(props) {
+    console.log(props)
     return (
         <p>Day Item</p>
     )
--- imports/ui/component/TripView.jsx
+++ imports/ui/component/TripView.jsx
@@ -3,14 +3,37 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Trips } from '../../api/trips';
 import DayItem from './DayItem';
 
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function getDates(startDate, endDate) {
+    var dateArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= endDate) {
+        dateArray.push(new Date(currentDate));
+        currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+}
+
+
 function TripView(props) {
-    console.log(props)
     if (props.trip) {
+
+        const startDate = new Date(props.trip.startDate)
+        const endDate = new Date(props.trip.endDate)
+        const dateArray = getDates(startDate, endDate)
+
         return (
             <div>
                 <h2>{props.trip.name}</h2>
                 <h3>{props.trip.location}</h3>
-                {/* <DayItem {...props} /> */}
+                {dateArray.map((trip, index) => {
+                    return <DayItem key={index} {...props} />
+                })}
             </div>
         );
     } else {
